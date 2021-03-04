5a8f3f0e0dd4bbe2b1c6e03d4fdb74ad2bd158b5
yh08037 <yh08037@gmail.com>
2021-03-04 09:40:35 +0900

    upload programming assingnment 1

---
+++ assignment-1/template_read_iris_v2.py
@@ -0,0 +1,30 @@
+# ID: ELEC946
+# NAME: Intelligent System Design
+# File name: template_read_iris_v2.py
+# Platform: Python 3.5.2 on Ubuntu Linux 16.04
+# Required Package(s): sys numpy 
+
+##############################################################
+# Template file for homework programming assignment 1
+# Modify the first 5 lines according to your implementation
+# This file is just for an example. Feel free to modify it.
+##############################################################
+
+##############################################################
+# NOTE: import sys and numpy only. 
+# No other packages are allowed to be imported
+##############################################################
+import sys
+import numpy as np
+
+if len(sys.argv) < 2:
+    print('usage: ' + sys.argv[0] + ' text_file_name')
+else:
+    ##############################################################
+    # WRITE YOUR OWN CODE LINES
+    # - open the input file, without pandas or csv packages
+    # - read header line
+    # - read data and class labels
+    # - compute mean and standard deviation
+    # - disply them 
+    ##############################################################
